cdf160f43c6b7536ee1b2b01eeb735b85fba86be
Tobias Wallura <tobias@wallura.org>
2017-03-23 21:37:08 +0100

    add deployment configuration via travis

--- config/lambda.config.js
+++ config/lambda.config.js
@@ -1,11 +1,23 @@
+import process from 'process';
+
 const { name, description, version } = require('../package.json');
 
+let accessKeyId, secretAccessKey, role, region;
+if (process.env.CI) {
+  accessKeyId = process.env.aws_accessKeyId;
+  secretAccessKey = process.env.aws_secretAccessKey;
+  role = process.env.aws_role;
+  region = process.env.aws_region;
+} else {
+  ({ accessKeyId, secretAccessKey, role, region } = require('./aws.config.json'));
+}
+
 export default {
-  accessKeyId: '',
-  secretAccessKey: '',
-  role: '',
+  accessKeyId: accessKeyId,
+  secretAccessKey: secretAccessKey,
+  role: role,
   functionName: name,
   description: `${description} (version ${version})`,
-  region: '',
+  region: region,
   handler: 'index.handler'
 };
